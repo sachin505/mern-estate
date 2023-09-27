01c27dccd280bfcee3951de97138d8ca690281cd
sachin505 <sachinallugani@gmail.com>
2023-09-28 00:40:20 +0530

    create header and links

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,9 +5,11 @@ import Sign_Out from './pages/Sign_Out'
 import About from './pages/About'
 import Home from './pages/Home'
 import Profile from './pages/Profile'
+import Header from './components/Header'
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/sign-in' element={<Sign_In />} />

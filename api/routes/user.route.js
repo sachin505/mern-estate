890b78a9b08b6c1bfcd54c010c9a6aed1134c013
sachin505 <sachinallugani@gmail.com>
2023-10-07 00:15:14 +0530

    full listing page and add contact to landlord functionality finished

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import {deleteUser, findAllUsers, getUserListings, updateUser} from '../controllers/user.controller.js'
+import {deleteUser, findAllUsers, getUser, getUserListings, updateUser} from '../controllers/user.controller.js'
 import { verifyToken } from '../utils/VerifyUser.js';
 
 const userRouter=express.Router();
@@ -8,5 +8,6 @@ userRouter.get('/findUsers',findAllUsers);
 userRouter.post('/update/:id',verifyToken,updateUser);
 userRouter.delete('/delete/:id',verifyToken,deleteUser);
 userRouter.get('/listings/:id',verifyToken,getUserListings);
+userRouter.get('/:id',verifyToken,getUser)
 
 export default userRouter;

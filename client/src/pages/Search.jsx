6064c408792e01f7ef20af3d994bfe0449accb64
sachin505 <sachinallugani@gmail.com>
2023-10-14 12:44:33 +0530

    create the listing by the components

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import {useNavigate} from 'react-router-dom'
+import ListingItem from '../components/ListingItem';
 function Search() {
     const navigate=useNavigate();
     const [sidebardata,setSidebardata]=useState({
@@ -53,7 +54,8 @@ function Search() {
         };
         fetchListings();
     },[location.search])
-    
+
+    console.log(listings);
     const handleChange=(e)=>{
         if(e.target.id==='all' || e.target.id === 'rent' || e.target.id==='sale'){
             setSidebardata({...sidebardata,type:e.target.id});
@@ -165,8 +167,17 @@ function Search() {
                 <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95'>search</button>
             </form>
         </div>
-        <div className="">
+        <div className="flex-1">
             <h1 className='text-3xl font-semibold border-b p-3 text-slate-700 mt-5'>Listing results :</h1>
+            <div className='p-7 flex-wrap gap-4'>
+                {!loading && listings.length===0 && (
+                    <p className='text-xl text-slate-700 peer-default:'>No listing found !</p>
+                )}
+                {loading && (
+                    <p className='text-xl text-slate-700 text-center w-full'>Loading...</p>
+                )}
+                {!loading && listings && listings.map(listing=><ListingItem key={listing._id} listing={listing}/>)}
+            </div>
         </div>
     </div>
   )
